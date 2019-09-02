afea149d8940117a44506e190ef421ddcda57738
Kaue Nicoletti Alves <dkauealves2@gmail.com>
2019-09-02 20:00:55 -0300

    SASS

--- grimorie-online-ok/src/main.js
+++ grimorie-online-ok/src/main.js
@@ -1,12 +1,32 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// Vue
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+// Bootstrap
+import 'jquery';
+import 'popper.js'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Fontawsome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faBorderAll, faSitemap, faTable, faFileAlt, faSearch, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Personalizado
+import './scss/app.scss'
+
+
+window.$ = require('jquery')
+library.add(faUserSecret, faBorderAll, faTable, faFileAlt, faSitemap, faSearch, faSignOutAlt);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
